6df9f26136caf5aeae9574296f23aca15a9a073c
Wilmer Gulcochia <karlwgs1989@gmail.com>
2026-06-20 23:53:32 -0500

    feat(prestamos): implementar encolarSolicitud para cola de solicitudes

--- prestamos.cpp
+++ prestamos.cpp
@@ -1,4 +1,5 @@
 #include "prestamos.h"
+#include "estructuras.h"
 #include <iostream>
 
 using namespace std;
@@ -7,6 +8,21 @@ using namespace std;
 
 void encolarSolicitud(NodoPrestamo* &frente, NodoPrestamo* &fin, Prestamo p) {
     // TODO: Wilmer (Integrante 3) debe implementar encolar (push_back) al final de la cola.
+    // 1. crear el nuevo nodo
+    NodoPrestamo* nuevoNodo = new NodoPrestamo();
+    nuevoNodo->dato = p;
+    nuevoNodo->siguiente = nullptr;
+
+    // 2. insertar en la cola
+    if (frente == nullptr) {
+        // si la cola está vacía, el nuevo nodo es tanto el frente como el fin
+        frente = nuevoNodo;
+        fin = nuevoNodo;    
+    } else {
+        // si no está vacía, se enlaza al final de la cola y se actualiza el puntero fin
+        fin->siguiente = nuevoNodo;
+        fin = nuevoNodo;
+    }
 }
 
 Prestamo desencolarSolicitud(NodoPrestamo* &frente, NodoPrestamo* &fin) {
